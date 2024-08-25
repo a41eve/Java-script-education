620fe60fbaf4e0a3b80525eb156d31004e1cff0e
Minaru <veter.210703@gmail.com>
2024-08-25 21:12:11 +0500

    Function - exercise four

--- alert.js
+++ alert.js
@@ -462,3 +462,25 @@ alert('Я, JavaScript!');
 		}
 	}
 })();
+
+(function Function_exercise_four() {
+	"use strict";
+
+	function pow(x, n) {
+		let result = x;
+
+		for (let i = 1; i < n; i++) {
+			result *= x;
+		}
+		return result;
+	}
+
+	let x = prompt('Введите число x: ', '');
+	let n = prompt('Введите число n: ', '');
+
+	if (n >= 1 && n % 1 == 0) {
+		alert(pow(x, n));
+	} else {
+		alert(`Степень ${n} не подходит, нужно натуральное значение`);
+	}
+})();
