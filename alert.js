9fd5fbc93784c62f45f51d20ee9087227ba7cae3
Minaru <veter.210703@gmail.com>
2024-08-25 20:24:58 +0500

    Cycles - exercise two

--- alert.js
+++ alert.js
@@ -310,3 +310,13 @@ alert('Я, JavaScript!');
 		alert( i-- ); // 3 -> 2 -> 1
 	}
 })();
+
+(function Cycles_exercise_two() {
+	"use strict";
+
+	let i = 0;
+	while (++i < 5) alert(i); // 1 -> 2 -> 3 -> 4
+
+	let j = 0;
+	while (j++ < 5) alert(j); // 1 -> 2 -> 3 -> 4 -> 5
+})();
