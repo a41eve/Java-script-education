624b3e34a183b4141d3495b4af0b94e02aaf97c8
Minaru <veter.210703@gmail.com>
2024-08-25 20:47:11 +0500

    Switch - exercise one

--- alert.js
+++ alert.js
@@ -375,3 +375,21 @@ alert('Я, JavaScript!');
 		alert(i);
 	}
 })();
+
+// switch 
+(function Switch_exercise_one() {
+	"use strict";
+
+	let browser;
+
+	if (browser == 'Edge') {
+		alert("You've got the Edge!");
+	} else if (browser == 'Chrome'
+		|| browser == 'Firefox'
+		|| browser == 'Safari'
+		|| browser == 'Opera' ) {
+			alert("Okay we support these browsers too");
+	} else {
+		alert("We hope that this page looks ok!");
+	}
+})();
