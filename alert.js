3148dfc2eabf3d1367725c34b65ef7de5afd8ba1
Minaru <veter.210703@gmail.com>
2024-08-25 19:54:38 +0500

    Logic_op - exercise nine

--- alert.js
+++ alert.js
@@ -239,3 +239,11 @@ alert('Я, JavaScript!');
 		alert('Не норм');
 	}
 })();
+
+(function If_exercise_nine() {
+	"use strict";
+
+	if (-1 || 0) alert( 'first' ); // выполнится
+	if (-1 && 0) alert( 'second' ); // не выполнится 
+	if (null || -1 && 1) alert( 'third' ); // выполнится 
+})();
