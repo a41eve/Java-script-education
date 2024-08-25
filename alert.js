ccc953f689b8b87acbc8350419732137d2d2d254
Minaru <veter.210703@gmail.com>
2024-08-25 18:44:54 +0500

    Variables - exercise one

--- alert.js
+++ alert.js
@@ -1,1 +1,14 @@
+// hello_w
 alert('Я, JavaScript!');
+
+// variables
+(function Variables_exercise_one() {
+	"use strict";
+
+		let admin;
+		let name = "Джон";
+
+		admin = name;
+
+		alert(admin);
+})();
